a59b2cdebe93118723c128199a8797cb822accfc
Manav Bhatia <manavbhatia@Manavs-MacBook-Pro.local>
2020-11-13 13:17:11 -0800

    new changes for Robin (static)

--- coalizerViz.js
+++ coalizerViz.js
@@ -25,7 +25,7 @@ var partyLabelTT = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-d3.json("sweden2014ches.json").then(function(nodes){
+d3.json("bundestag2009wom.json").then(function(nodes){
   var blocks = nodes.length;
   
   var aScale = d3.scaleLinear().range([0.0, 700]).domain([0.0, 1.0]);
@@ -33,15 +33,29 @@ d3.json("sweden2014ches.json").then(function(nodes){
   var uScale = d3.scaleLinear().range([0.0, 450/blocks]).domain([1.0, 0.0]);
 
   var tickData = {
-    0: "0.0 / 1.0",
+    0: "1.0 / 0.0",
     0.5: "0.5",
-    1: "0.0 / 1.0"
+    1: "1.0 / 0.0"
+  }
+  
+  var bottomTickData = {
+    0: "0.0",
+    0.5: "0.5",
+    1: "1.0 / 0.0"
+  }
+  
+  var topTickData = {
+    0: "1.0 / 0.0",
+    0.5: "0.5",
+    1: "1.0"
   }
 
   var alphaAxis = d3.axisTop().scale(aScale).tickPadding(2);
   var betaAxis = d3.axisBottom().scale(bScale).tickPadding(2);
   var utilityAxis = d3.axisRight().scale(uScale).ticks(3).tickFormat(function(d){ return tickData[d] });
-
+  var utilityTopAxis = d3.axisRight().scale(uScale).ticks(3).tickFormat(function(d){ return topTickData[d] });
+  var utilityBottomAxis = d3.axisRight().scale(uScale).ticks(3).tickFormat(function(d){ return bottomTickData[d] });
+  
   var outline = svg.selectAll("rect")
         .data("A")
         .enter()
@@ -58,10 +72,31 @@ d3.json("sweden2014ches.json").then(function(nodes){
   var shadeColor = "white";
   nodes.forEach(function(d, i){
     xPos = 150;
-    svg.append("g")
+    if(i == 0){
+      var topAxis = svg.append("g")
+      .attr("class", "u axis")
+      .attr("transform", "translate(850," + yPos + ")")
+      .call(utilityTopAxis);
+      
+      topAxis.append("text")
+      .text("Utility")
+      .attr("fill", "black")
+      .attr("x", 200)
+      .attr("y", -60)
+      .style("font-size", 15)
+      .attr("transform", "rotate(90)");
+
+    } else if(i == (nodes.length-1)){
+      svg.append("g")
+      .attr("class", "u axis")
+      .attr("transform", "translate(850," + yPos + ")")
+      .call(utilityBottomAxis);
+    } else {
+      svg.append("g")
       .attr("class", "u axis")
       .attr("transform", "translate(850," + yPos + ")")
       .call(utilityAxis);
+    }
     d.coalitionNames.forEach(function(c, j){
       if(d.coalitionShade[j]){
         shadeColor = "lightgrey";
@@ -77,15 +112,16 @@ d3.json("sweden2014ches.json").then(function(nodes){
         .style("stroke-width", "1px")
         .style("stroke", "black");
       
-      if(d.ranges[j+1]-d.ranges[j] <= 0.05){
-          block.on("mouseover", function(d){
+      if(d.ranges[j+1]-d.ranges[j] <= 0.05 || d.coalitionShade[j]){
+          block.on("mouseover", function(e){
             console.log(c);
             partyLabelTT.transition()
             .duration(200)
             .style("opacity", 1); 
-            partyLabelTT.html("<p style='text-align:center;'>" + c + "<p/>")
+            partyLabelTT.html("<div class='ttip'><p style='text-align:center;'><b>" + c + "</b></p><p>Office Range: " + 
+                             d.ranges[j] + " - " + d.ranges[j+1] + "</p><p>Policy Range: " + (1 - d.ranges[j]).toFixed(2) + " - " + (1 - d.ranges[j+1]).toFixed(2) + "</p></div>")
                     //set x and y pos using style and use the d3.event to track pan drag zoom movement and update pos of tooltip
-                    .style("top", (d3.event.pageY - 10) + "px")		
+                    .style("top", (d3.event.pageY + 450/blocks) + "px")		
                     .style("left", (d3.event.pageX +10) + "px");	
             console.log(d3.event.pageY);
           })
@@ -134,7 +170,7 @@ d3.json("sweden2014ches.json").then(function(nodes){
         .call(alphaAxis);
   
     gA.append("text")
-        .text("α-Office")
+        .text("Office (α)")
         .attr("fill", "black")
         .attr("x", -50)
         .attr("y", 0)
@@ -148,10 +184,11 @@ d3.json("sweden2014ches.json").then(function(nodes){
         .call(betaAxis); 
   
     gB.append("text")
-        .text("β-Policy")
+        .text("Policy (β)")
         .attr("fill", "black")
         .attr("x", -50)
         .attr("y", 0)
         .style("font-size", 12);
+  
   
 })
